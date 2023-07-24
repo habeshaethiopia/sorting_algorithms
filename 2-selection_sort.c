21eb92781f6fd97c7456252f5161178bbf4fb45e
Adane Moges <adanemogs6@gmail.com>
2023-07-24 18:12:18 +0300

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,18 +7,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, temp, j;
-	int index;
+	size_t i, j;
+	int index, temp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-
-		for (j = i; j < size; j++)
-			if (array[i] > array[j])
-				index = j;
 		temp = array[i];
-		array[i] = array[index];
-		array[index] = temp;
-		print_array(array, size);
+		for (j = i + 1; j < size; j++)
+			if (temp > array[j])
+			{
+				temp = array[j];
+				index = j;
+			}
+		if (array[i] > array[index])
+		{
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
 	}
 }
