6fe3720c0f46f539cd5a8d276bddcbb3c912f1c2
Adane Moges <adanemogs6@gmail.com>
2023-07-25 14:06:59 +0300

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,7 @@
 #include "sort.h"
+
+void sort_q(int *arr, int l, int h, size_t n);
+
 /**
  * quick_sort - the implementation of quick_sort
  * @arr: the array
@@ -8,6 +11,14 @@ void quick_sort(int *arr, size_t n)
 {
 	sort_q(arr, 0, n - 1, n);
 }
+
+/**
+ * sort_q - recursive function to perform quick sort
+ * @arr: the array to sort
+ * @l: the lower index of the subarray
+ * @h: the higher index of the subarray
+ * @n: the size of the array
+ */
 void sort_q(int *arr, int l, int h, size_t n)
 {
 	int pivot;
@@ -18,19 +29,21 @@ void sort_q(int *arr, int l, int h, size_t n)
 		sort_q(arr, pivot + 1, h, n);
 	}
 }
+
 /**
- * partition - the function to partion the array and sort
- * @h: is the highest index
- * @l: the lowest index
- * @arr: the array
- * Return: the index of partion
+ * partition - function to partition the array and sort
+ * @arr: the array to sort
+ * @l: the lower index of the subarray
+ * @h: the higher index of the subarray
+ * @n: the size of the array
+ * Return: the index of the partition
  */
 int partition(int *arr, int l, int h, size_t n)
 {
 	int temp, pivot = arr[h];
 	int i = l - 1, j;
 
-	for (j = l; i < h; j++)
+	for (j = l; j < h; j++)
 	{
 		if (arr[j] <= pivot)
 		{
